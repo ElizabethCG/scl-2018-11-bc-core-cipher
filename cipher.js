52d54b72a4f717807462f3814f70b836c8766dee
ElizabethCG <elizabeth.caceres@gmail.com>
2018-11-28 20:04:16 -0300

    subiendo trabajo presentado

--- cipher.js
+++ cipher.js
@@ -1,8 +1,72 @@
 window.cipher = {
-  encode: () => {
-    /* Acá va tu código */
+  encode: (a,b) => {
+
+    // a=mensajeInic
+    // b=desplazamientoInic
+
+
+      let analisisMensaje=a.toUpperCase().trim();
+      let largoCadena=analisisMensaje.length;
+
+
+      var cifrado="";
+
+      for (let i=0; i<largoCadena; i++){
+
+        let caracterAscii=analisisMensaje.charCodeAt(i)
+
+
+        if(caracterAscii===32){caracterAscii=48;
+          cifrado=cifrado+"0";
+        }else {
+
+          cifrado=cifrado+(String.fromCharCode(((caracterAscii-65+b)%26)+65));
+
+          }}
+
+
+
+  return cifrado;
   },
-  decode: () => {
-    /* Acá va tu código */
+
+
+
+// FUNCION INDICADA
+  decode: (a,b) => {
+
+
+      let analisisMensaje=a.toUpperCase().trim();
+      let largoCadena=analisisMensaje.length;
+
+
+
+      var cifrado="";
+
+      for (let i=0; i<largoCadena; i++){
+
+        let caracterAscii=analisisMensaje.charCodeAt(i)
+
+
+        if(caracterAscii===48){caracterAscii=32;
+          cifrado=cifrado+" ";
+        }else {
+
+            var mirar =((caracterAscii-65-(b%26)));
+            if (mirar<0){
+              var nuevoval=26+(mirar);
+              mirar=String.fromCharCode(nuevoval+65);
+              cifrado=cifrado+mirar;
+              }
+
+            else{
+              cifrado=cifrado+(String.fromCharCode((caracterAscii-65-(b%26)+65)));
+
+            }
+          }
+
+          }
+
+    return cifrado;
+
   }
 };
--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-/* Acá va tu código */
+
+function escribirMensaje() {
+  let mensajeInic=document.getElementById("mensaje").value;
+  let desplazamientoInic=parseInt(document.getElementById("desplazamiento").value);
+  document.getElementById ("mensajeFinal").innerHTML ="<p2> El cifrado será: "    + (window.cipher.encode(mensajeInic,desplazamientoInic)) +     "</p2>";
+
+}
+
+
+// VER COMO ASIGNAR UN VALOR AL ONCLICK + LA LLAMADA DE UNA FUNCIÓN
+function descifrar() {
+  let mensajeInic=document.getElementById("mensaje").value;
+  let desplazamientoInic=parseInt(document.getElementById("desplazamiento").value);
+  document.getElementById ("mensajeFinal").innerHTML ="<p2> El cifrado será: "    + (window.cipher.decode(mensajeInic,desplazamientoInic)) +     "</p2>";
+
+  }
