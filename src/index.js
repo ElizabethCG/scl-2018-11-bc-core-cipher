d17fa1419c12c1677920485505ee3038cf8fccd1
ElizabethCG <elizabeth.caceres@gmail.com>
2018-11-30 21:24:30 -0300

    Elimina errores y corre test para cipher

--- src/index.js
+++ src/index.js
@@ -1,16 +1,20 @@
-
+// // window.onload=
 function escribirMensaje() {
   let mensajeInic=document.getElementById("mensaje").value;
   let desplazamientoInic=parseInt(document.getElementById("desplazamiento").value);
-  document.getElementById ("mensajeFinal").innerHTML ="<p2> Tu mensaje cifrado quedará así: "    + (window.cipher.encode(mensajeInic,desplazamientoInic)) +     "</p2>";
+  document.getElementById ("mensajeFinal").innerHTML ="<p2> Tu mensaje cifrado quedará así: "    + (cipher.encode(mensajeInic,desplazamientoInic)) +     "</p2>";
   document.getElementById("mensajeFinal").style.display="block";
+
+  return "prueba";
 }
 
 
+// window.onload=
 // VER COMO ASIGNAR UN VALOR AL ONCLICK + LA LLAMADA DE UNA FUNCIÓN
 function descifrar() {
   let mensajeInic=document.getElementById("mensaje").value;
   let desplazamientoInic=parseInt(document.getElementById("desplazamiento").value);
-  document.getElementById ("mensajeFinal").innerHTML ="<p2> Tu mensaje cifrado dice lo siguiente: "    + (window.cipher.decode(mensajeInic,desplazamientoInic)) +     "</p2>";
+  document.getElementById ("mensajeFinal").innerHTML ="<p2> Tu mensaje cifrado dice lo siguiente: "    + (cipher.decode(mensajeInic,desplazamientoInic)) +     "</p2>";
   document.getElementById("mensajeFinal").style.display="block";
-  }
+  return "prueba";
+}
