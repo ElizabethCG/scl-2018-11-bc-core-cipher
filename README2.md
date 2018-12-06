aaa82e991b2f72761c7ec6778301d1aaf901e706
ElizabethCG <elizabeth.caceres@gmail.com>
2018-12-05 21:22:20 -0300

    Recibe todos los caractes. Cifra solo mayusculas y minusculas

---
+++ README2.md
@@ -0,0 +1,53 @@
+# Proceso y decisiones de diseño.
+## Consideraciones generales
+### General
+[cifrado César](https://en.wikipedia.org/wiki/Caesar_cipher)
+
+Proporciona calma.
+Muchas marcas lo utilizan para representar la creatividad, la imaginación y la sabiduría.
+Se asocia a lo femenino, a la magia y la espiritualidad.
+
+[![tips caesar cipher](https://img.youtube.com/vi/zd8eVrXhs7Y/0.jpg)](https://www.youtube.com/watch?v=zd8eVrXhs7Y)
+
+[ ] `README.md` casilla verificación
+_offset_  cursivas?
+_**strings**_
+cursivas y negrita
+
+- (viñetas).
+**HTML** negritas
+
+* `README.md`: destacado y viñeta
+
+
+
+
+
+
+
+# Test aplicados:
+
+cipher
+  √ debería ser un objeto
+  cipher.encode
+    √ debería ser una función
+    √ debería retornar "HIJKLMNOPQRSTUVWXYZABCDEFG" para "ABCDEFGHIJKLMNOPQRSTUVWXYZ" con offest 33
+  cipher.decode
+    √ debería ser una función
+    √ debería retornar "ABCDEFGHIJKLMNOPQRSTUVWXYZ" para "HIJKLMNOPQRSTUVWXYZABCDEFG" con offest 33
+
+
+5 passing (23ms)
+
+-----------|----------|----------|----------|----------|-------------------|
+File       |  % Stmts | % Branch |  % Funcs |  % Lines | Uncovered Line #s |
+-----------|----------|----------|----------|----------|-------------------|
+All files  |    84.62 |    66.67 |      100 |    91.67 |                   |
+cipher.js  |    84.62 |    66.67 |      100 |    91.67 |             10,28 |
+-----------|----------|----------|----------|----------|-------------------|
+
+
+# Quiénes son los usuarios y su relación con el producto.
+
+
+# Cómo el producto soluciona los problemas/necesidades de los usuarios.
--- src/cipher.js
+++ src/cipher.js
@@ -1,72 +1,66 @@
-window.cipher = {
-  encode: (a,b) => {
+  window.cipher = {
+    encode: (strng,offset) => {
+        let analisisMensaje=strng.trim();
+        let largoCadena=analisisMensaje.length;
+        var cifrado="";
 
-    // a=mensajeInic
-    // b=desplazamientoInic
+                for (let i=0; i<largoCadena; i++){
+                  let caracterAscii=analisisMensaje.charCodeAt(i)
 
+                  if(64<caracterAscii&&caracterAscii<91||96<caracterAscii&&caracterAscii<123){
+                    if (caracterAscii<91){cifrado=cifrado+(String.fromCharCode(((caracterAscii-65+offset)%26)+65))}
+                                     else{cifrado=cifrado+(String.fromCharCode(((caracterAscii-97+offset)%26)+97))}
 
-      let analisisMensaje=a.toUpperCase().trim();
-      let largoCadena=analisisMensaje.length;
+                  }else{
 
+                    cifrado=cifrado+analisisMensaje[i];
+                  }
 
-      var cifrado="";
-
-      for (let i=0; i<largoCadena; i++){
-
-        let caracterAscii=analisisMensaje.charCodeAt(i)
-
-
-        if(caracterAscii===32){caracterAscii=48;
-          cifrado=cifrado+"0";
-        }else {
-
-          cifrado=cifrado+(String.fromCharCode(((caracterAscii-65+b)%26)+65));
-
-          }}
+                }
+      return cifrado;
+            },
 
 
 
-  return cifrado;
-  },
+
+          decode: (strng,offset) => {
+              let analisisMensaje=strng.trim();
+              let largoCadena=analisisMensaje.length;
+              var cifrado="";
+
+              for (let i=0; i<largoCadena; i++){
+                let caracterAscii=analisisMensaje.charCodeAt(i)
 
 
+            if(64<caracterAscii&&caracterAscii<91||96<caracterAscii&&caracterAscii<123){
+            if (caracterAscii<91){
 
-// FUNCION INDICADA
-  decode: (a,b) => {
-
-
-      let analisisMensaje=a.toUpperCase().trim();
-      let largoCadena=analisisMensaje.length;
-
-
-
-      var cifrado="";
-
-      for (let i=0; i<largoCadena; i++){
-
-        let caracterAscii=analisisMensaje.charCodeAt(i)
-
-
-        if(caracterAscii===48){caracterAscii=32;
-          cifrado=cifrado+" ";
-        }else {
-
-            var mirar =((caracterAscii-65-(b%26)));
-            if (mirar<0){
-              var nuevoval=26+(mirar);
-              mirar=String.fromCharCode(nuevoval+65);
-              cifrado=cifrado+mirar;
-              }
-
-            else{
-              cifrado=cifrado+(String.fromCharCode((caracterAscii-65-(b%26)+65)));
-
+              var fueraDeRango =((caracterAscii-65-(offset%26)));
+              if (fueraDeRango<0){
+                var nuevoval=26+(fueraDeRango);
+                fueraDeRango=String.fromCharCode(nuevoval+65);
+                cifrado=cifrado+fueraDeRango;
+              }else{cifrado=cifrado+(String.fromCharCode((caracterAscii-65-(offset%26)+65)))}
             }
+
+              else{
+                 fueraDeRango =((caracterAscii-97-(offset%26)));
+                if (fueraDeRango<0){
+                  nuevoval=26+(fueraDeRango);
+                  fueraDeRango=String.fromCharCode(nuevoval+97);
+                  cifrado=cifrado+fueraDeRango;
+
+              }else {cifrado=cifrado+(String.fromCharCode((caracterAscii-97-(offset%26)+97)))}}
+
+
+
+
+          }else{
+
+            cifrado=cifrado+analisisMensaje[i];
           }
 
-          }
+        }return cifrado;
+}
 
-    return cifrado;
-
-  }
-};
+}
